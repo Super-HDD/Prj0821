70b450c33250ec23f3f0411d49e65f0d9208afa4
SuperHDD <142251648+Super-HDD@users.noreply.github.com>
2023-08-22 17:26:57 +0800

    pet editor demo

--- rust/src/mini_app.rs
+++ rust/src/mini_app.rs
@@ -1,32 +1,24 @@
 
 
-// use godot::prelude::*;
-// use godot::engine::{TextureButton,TextureButtonVirtual,Label};
+use godot::prelude::*;
+use godot::engine::{TextureButton,TextureButtonVirtual};
 
-// #[derive(GodotClass)]
-// #[class(base=TextureButton)]
-// pub struct MiniApp {
-//     //app_label:Label,
-//     app_behavior:Box<dyn MiniAppVirtual>,
-//     #[base]
-//     base: Base<TextureButton>,
-// }
+#[derive(GodotClass)]
+#[class(base=TextureButton)]
+pub struct MiniApp {
+    //app_label:Label,
+    #[base]
+    base: Base<TextureButton>,
+}
 
-// #[godot_api]
-// impl TextureButtonVirtual for MiniApp{
-//     fn init(base: Base<TextureButton>) -> Self {
-//         godot_print!("MiniApp loaded");
-//         Self {     
-//             base
-//         }
-//     }
+#[godot_api]
+impl TextureButtonVirtual for MiniApp{
+    fn init(base: Base<TextureButton>) -> Self {
+        godot_print!("MiniApp loaded");
+        Self {     
+            base
+        }
+    }
 
-//     fn pressed(&mut self){
-//         let mut base=self;
-//        // MiniAppVirtual::on_button_pressed(mut base);
-//     }
-// }
+}
 
-// pub trait MiniAppVirtual{
-//     fn on_button_pressed<T>(base:&mut Base<T>); 
-// }
